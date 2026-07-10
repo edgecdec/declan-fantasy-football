28c5b7a17428588c4303026af2b7f9976a70420f
Declan Edgecombe <edeclan@amazon.com>
2026-07-10 15:58:23 -0700

    Raise deploy build heap from 1536MB to 3072MB
    Next 16.2.10 build OOMed at 1536MB on the VPS (1.9GB RAM, 2GB swap),
    leaving .next empty and putting pm2 into a 25k-restart loop. Verified
    manually that 3072MB completes with current app fleet.

--- deploy_webhook.sh
+++ deploy_webhook.sh
@@ -32,7 +32,7 @@ if [ "$OLD_PKG_HASH" != "$NEW_PKG_HASH" ]; then
 fi
 
 rm -rf .next
-export NODE_OPTIONS='--max-old-space-size=1536'
+export NODE_OPTIONS='--max-old-space-size=3072'
 npm run build
 
 pm2 restart "$PM2_NAME"
